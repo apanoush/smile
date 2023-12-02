52f969866a6172d14a7027d7aee89e98db07cc50
OlivierMARIN17 <olivier.marin-cudraz@epfl.ch>
2023-12-02 17:07:35 +0100

    okok

--- App/App.js
+++ App/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>SMILE, Votre réseau</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'center',
   },
 });
