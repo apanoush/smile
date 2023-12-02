60ec098d4aebe4be73ca6438bb006059572e8e6b
OlivierMARIN17 <olivier.marin-cudraz@epfl.ch>
2023-12-02 23:07:09 +0100

    first page

--- App/App.js
+++ App/App.js
@@ -1,24 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import NamePrompt from './petits codes/name prompt';
+import { View, Image, StyleSheet,Text } from 'react-native';
+
 
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>SMILE, Votre réseau</Text>
-      <StatusBar style="auto" />
-      <NamePrompt />
+      
+      {/*HI*/}
+      <View style={styles.TitleContainer}>
+      <Text style = {styles.Title}>HI</Text>
+      </View>
+      {/*Soleil*/}
+        <View style={styles.SoleilContainer}>
+        <Image 
+        source={require('./assets/sun.png')}
+        style={styles.soleil}
+        resizeMode="contain"
+        />
+        </View>
+      {/*Welcome to Smile*/}
+      <View style={styles.TextContainer}>
+      <Text style = {styles.Title}> Welcome{'\n'}       on{'\n'}   SMILE</Text>
+      </View>
+
     </View>
 
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'flex-start',
+    backgroundColor: '#FFF1A6',
+    alignItems: 'center',
     justifyContent: 'center',
   },
+  Title : {
+    color: '#000000',
+    fontSize: 40,
+    fontFamily: "Georgia"
+  },
+  TitleContainer:{
+    position: "absolute",
+    textAlign: "center",
+    top: "25%",
+  },
+  TextContainer:{
+    position: "absolute",
+    textAlign: "center",
+    top: "45%",
+  },
+  SoleilContainer:{
+    backgroundColor: 'transparent',
+    position: "absolute",
+    textAlign: "center",
+    top: "32%",
+  },
+  soleil: {
+    width: 200, // Adjust width and height according to your image size
+    height: 100,
+    backgroundColor: 'transparent'
+  }
 });
+
+
