954b159a14b1abde4ec081db3cfa3080f01c30bb
OlivierMARIN17 <olivier.marin-cudraz@epfl.ch>
2023-12-02 23:58:14 +0100

    oeooeeee

--- App/App.js
+++ App/App.js
@@ -1,9 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { View, Image, StyleSheet,Text } from 'react-native';
-import NamePrompt from './petits codes/name prompt';
+
+import { View, Image, StyleSheet,Text, TextInput } from 'react-native';
+import React, { useState } from 'react';
 
 
 export default function App() {
+ const [name, setName] = useState('olivier');
 
   return (
     <View style={styles.container}>
@@ -23,9 +24,17 @@ export default function App() {
       {/*Welcome to Smile*/}
       <View style={styles.TextContainer}>
       <Text style = {styles.Title}> Welcome{'\n'}       on{'\n'}   SMILE</Text>
-
-      <NamePrompt />
       </View>
+      {/*Prompt*/}
+      <View style={styles.prompt}>
+        <Text style = {styles.simpleText}> Enter your beautiful name :</Text>
+        <TextInput 
+        style={styles.input}
+        placeholder='e.g. Olivier'
+        onChangeText={(text) => setName(text)}/>
+      </View>
+
+      
 
     </View>
 
@@ -48,24 +57,42 @@ const styles = StyleSheet.create({
   TitleContainer:{
     position: "absolute",
     textAlign: "center",
-    top: "25%",
+    top: "15%",
   },
   TextContainer:{
     position: "absolute",
     textAlign: "center",
-    top: "45%",
+    top: "38%",
   },
   SoleilContainer:{
     backgroundColor: 'transparent',
     position: "absolute",
     textAlign: "center",
-    top: "32%",
+    top: "26%",
   },
   soleil: {
     width: 200, // Adjust width and height according to your image size
     height: 100,
     backgroundColor: 'transparent'
-  }
+  },
+  prompt: {
+    position: "absolute",
+    textAlign: "center",
+    top: "60%",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  simpleText: {
+    fontSize: 25,
+    color: '#000000',
+    textAlign: 'center',
+    fontFamily: "Georgia"
+  },
+
 });
 
 
