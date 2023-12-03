1f82d7eaaf2ab42f149639c98af3c07b2a8266ea
OlivierMARIN17 <olivier.marin-cudraz@epfl.ch>
2023-12-03 01:37:58 +0100

    marche

--- App/App.js
+++ App/App.js
@@ -1,73 +1,19 @@
 
-import { View, Image, StyleSheet,Text, TextInput } from 'react-native';
 import React, { useState } from 'react';
-import welcome_page from './petits codes/welcome page';
+import Welcome from './petits codes/welcome page.js';
 
 
 export default function App() {
-  const [name, setName] = useState('olivier');
-  welcome_page();
 
   return (
-    <View>
-      <welcome_page />
-    </View>
+   <Welcome></Welcome>
+   
+    
 
   );
 }
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF1A6',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  Title : {
-    color: '#000000',
-    fontSize: 40,
-    fontFamily: "Georgia"
-  },
-  TitleContainer:{
-    position: "absolute",
-    textAlign: "center",
-    top: "15%",
-  },
-  TextContainer:{
-    position: "absolute",
-    textAlign: "center",
-    top: "38%",
-  },
-  SoleilContainer:{
-    backgroundColor: 'transparent',
-    position: "absolute",
-    textAlign: "center",
-    top: "23%",
-  },
-  soleil: {
-    width: 200, // Adjust width and height according to your image size
-    height: 100,
-    backgroundColor: 'transparent'
-  },
-  prompt: {
-    position: "absolute",
-    textAlign: "center",
-    top: "60%",
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-  simpleText: {
-    fontSize: 25,
-    color: '#000000',
-    textAlign: 'center',
-    fontFamily: "Georgia"
-  },
 
-});
 
 
--- App/petits codes/welcome page.js
+++ App/petits codes/welcome page.js
@@ -1,7 +1,7 @@
 import { View, Image, StyleSheet,Text, TextInput } from 'react-native';
 import React, { useState } from 'react';
 
-export default function welcome_page() {
+const Welcome = ({firstVisit}) => {
     const [name, setName] = useState('olivier');
 
     return (
@@ -39,3 +39,56 @@ export default function welcome_page() {
     );
 
 };
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#FFF1A6',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    Title : {
+      color: '#000000',
+      fontSize: 40,
+      fontFamily: "Georgia"
+    },
+    TitleContainer:{
+      position: "absolute",
+      textAlign: "center",
+      top: "15%",
+    },
+    TextContainer:{
+      position: "absolute",
+      textAlign: "center",
+      top: "38%",
+    },
+    SoleilContainer:{
+      backgroundColor: 'transparent',
+      position: "absolute",
+      textAlign: "center",
+      top: "23%",
+    },
+    soleil: {
+      width: 200, // Adjust width and height according to your image size
+      height: 100,
+      backgroundColor: 'transparent'
+    },
+    prompt: {
+      position: "absolute",
+      textAlign: "center",
+      top: "60%",
+    },
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+    simpleText: {
+      fontSize: 25,
+      color: '#000000',
+      textAlign: 'center',
+      fontFamily: "Georgia"
+    },
+  
+  });
+export default Welcome;
